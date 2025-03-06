8ba15dcf0cdc7a568c761b326b5502fc8ad01453
Milan <milan.zweit@gmail.com>
2025-03-06 22:27:25 +0100

    Added ednpoints

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 
-app = FastAPI(root_path="/user")
+app = FastAPI()
 
+@app.get("/answer")
+def read_root():
+    return {"Hello": "World"}
 
-@app.get("/")
+@app.post("/preferences/init")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/preferences/username")
+def read_root():
+    return {"Hello": "World"}
+
+@app.update("/preferences/username")
 def read_root():
     return {"Hello": "World"}
