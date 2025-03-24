e7a14238ded5e2ea701456337b1405c35ad33e80
Milan <milan.zweit@gmail.com>
2025-03-24 09:44:32 +0100

    get_information can now handle more than one value per information

--- backend/llm_fetchers/UseCases.py
+++ backend/llm_fetchers/UseCases.py
@@ -20,6 +20,7 @@ def get_information(user_input, information_needed) -> str:
         "Give back a list of information provided by the user."
         f"Base the list on the information needed: {information_needed} "
         "Only give back the lists in this format [information_needed: <value>,...] if there is no value just leave it empty"
+        "If there is more than one value for one info return them like this [<value>,<value>]"
     )
     return chatGPTProcessor.process_input(prompt)
 
@@ -36,7 +37,7 @@ def response(user_input, api_calls) -> str:
     return chatGPTProcessor.process_input(prompt)
 
 if __name__ == "__main__":
-    user_input = "I want to know the weather in Stuttgart. I want to know the BBC news. I want to know about my Stocks."
+    user_input = "I want to know the weather in Stuttgart and I want to know the BBC and ARD news also tell me about my Stocks."
     use_case = declare_usecase(user_input)
     information_needed = "City, News Service, Stocks"
     information_got = get_information(user_input, information_needed)
