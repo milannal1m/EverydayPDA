57a1113d25e7376a449bdd159155a5f488e5445f
TheLordNight <117924447+TheLordNight@users.noreply.github.com>
2025-04-11 18:53:53 +0200

    Fixed answer_processor

--- backend/api/answer_processor.py
+++ backend/api/answer_processor.py
@@ -1,17 +1,25 @@
 import os
 import sys
 import traceback
+import logging
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from api.usecase_handler import UseCaseHandler
 from api.summary_generator import UserSummaryGenerator
 from api.database_utils import get_all_users
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 class AnswerProcessor:
     async def get_answer(self, message: str, user_id: str):
         use_cases, info = await UseCaseHandler().get_use_cases_and_info(message, user_id)
+        logger.info(f"Use Cases: {use_cases}, Info: {info}")
         api_data = UseCaseHandler().call_apis(use_cases, info)
-        return {"response": UseCaseHandler().get_response(message, api_data)}
+        logger.info(f"API Data: {api_data}")
+        response = UseCaseHandler().get_response(message, api_data)
+        logger.info(f"Response: {response}")
+        return {"response": response}
 
     async def get_morning(self):
         user_ids = await get_all_users()
